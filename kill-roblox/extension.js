d253145cf02bda5300ad7b37fa3a6adff77dca99
iNanoToxin <dylanleodaniel@gmail.com>
2023-02-28 19:48:31 -0500

    Update

--- kill-roblox/extension.js
+++ kill-roblox/extension.js
@@ -3,19 +3,15 @@ const vscode = require('vscode');
 
 const myCommandId = 'kill-roblox.kill';
 
-let kill_command = vscode.commands.registerCommand(myCommandId, () => {
-	process.exec('taskkill /f /im RobloxPlayerBeta.exe');
-});
-
-let kill_button = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-
 function activate(context) {
-	kill_button.text = 'Kill roblox';
+	console.log("activate");
+	context.subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+		process.exec('taskkill /f /im RobloxPlayerBeta.exe');
+	}));
+	let kill_button = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
 	kill_button.command = myCommandId;
+	kill_button.text = '$(debug-disconnect) Kill roblox';
 	kill_button.show();
-
-	context.subscriptions.push(kill_button);
-	context.subscriptions.push(kill_command);
 }
 
 function deactivate() {}
